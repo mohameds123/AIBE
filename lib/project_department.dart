eb64431c3d4753ae6b0cef2994832483db79e596
mohameds123 <za.mohamed011213@gmail.com>
2021-10-06 20:36:59 +0200

    saleh commit

--- lib/project_department.dart
+++ lib/project_department.dart
@@ -8,145 +8,319 @@ class ProjectDepartment_Screen extends StatelessWidget {
         elevation: 20.0,
         backgroundColor:HexColor('2A0934'),
         title: Text(
-          'AIBE',
+          'Project Departments',
           style: TextStyle(
-            fontSize: 25.0,
+            fontSize: 20.0,
           ),
         ),
       ),
-      body: Container(
-        width: double.infinity,
-        color: HexColor('2A0934'),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Hello AIBErs',
-                    style: TextStyle(
-                      color: Colors.blueAccent,
-                      fontSize: 20.0,
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          color: HexColor('2A0934'),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Our First Track',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 20.0,
+                      ),
                     ),
+                    Spacer(),
+                    Image.asset(
+                      'images/x2.png',
+                      width: 35.0,
+                      height: 35.0,
+                      fit: BoxFit.cover,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 150,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Human Resources(HR)',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x8.jpg',
+                              width: 150,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
                   ),
-                  Spacer(),
-                  Image.asset(
-                    'images/x2.png',
-                    width: 35.0,
-                    height: 35.0,
-                    fit: BoxFit.cover,
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Center(
-              child: Text(
-                'AIBE Two Tracks',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize:20.0,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 18.0,
-            ),
-            Center(
-              child: MaterialButton(
-                onPressed: (){},
-                child: Container(
-                    width: 250.0,
-                    height: 200.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.white
-                    ),
-                    child:Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Container(
-                            width: 150,
-                            height: 30,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                color: Colors.black.withOpacity(0.8)
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Project Departments',
-                                style: TextStyle(
-                                  color: Colors.white,
+              SizedBox(height: 16.0,),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 140,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Public Relations(PR)',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(60.0),
-                          child: Image.asset(
-                            'images/x3.jpg',
-                            width: 150,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      ],
-                    )
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x7.jpg',
+                              width: 120,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 16.0,),
-            Center(
-              child: MaterialButton(
-                onPressed: (){},
-                child: Container(
-                    width: 250.0,
-                    height: 200.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.white
-                    ),
-                    child:Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Container(
-                            width: 120,
-                            height: 30,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20.0),
-                                color: Colors.black.withOpacity(0.8)
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Academic fields',
-                                style: TextStyle(
-                                  color: Colors.white,
+              SizedBox(height: 16.0,),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 120,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Operation',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(60.0),
-                          child: Image.asset(
-                            'images/x4.jpg',
-                            width: 150,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      ],
-                    )
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x10.jpg',
+                              width: 150,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 16.0,),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 120,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Coordination',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x9.jpg',
+                              width: 150,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0,),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 120,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Advertising',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x11.jpg',
+                              width: 150,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0,),
+              Center(
+                child: MaterialButton(
+                  onPressed: (){},
+                  child: Container(
+                      width: 250.0,
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white
+                      ),
+                      child:Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                              width: 120,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  color: Colors.black.withOpacity(0.8)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Media Production',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(60.0),
+                            child: Image.asset(
+                              'images/x12.jpg',
+                              width: 150,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      )
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
